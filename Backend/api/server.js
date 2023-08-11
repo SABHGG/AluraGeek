3d0869e4d0dfb88b83cbfbfeb26ae207d8616f0b
Sergio Beleño <sergiosabh05@gmail.com>
2023-08-11 15:02:14 -0500

    server modificado

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -6,7 +6,12 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
-
+server.use(
+  jsonServer.rewriter({
+    "/api/categorias/*": "/categorias/$1",
+    "/api/productos/*": "/productos/$1",
+  })
+);
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
