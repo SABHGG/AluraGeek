bd88b8418d104a2567e37b2c963753b4c0d12d1d
Sergio Beleño <sergiosabh05@gmail.com>
2023-08-15 17:36:41 -0500

    update proxy vite

--- AluraGeek/vite.config.js
+++ AluraGeek/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  proxy: {
+    '/productos': {
+      target: 'http://localhost:3000/	',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/productos/, ''),
+    },
+  },
 })
