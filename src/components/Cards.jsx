3c32003bbe18916830247afdb61f3f7252ffa1c6
Sergio Beleño <sergiosabh05@gmail.com>
2023-08-18 17:01:24 -0500

    update arrow img

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,8 +1,9 @@
 import Card from "./Card";
 import { getAllProducts } from "../services/client-service";
 import { useState, useEffect } from "react";
-import { PropTypes } from "prop-types"
+import { PropTypes } from "prop-types";
 import Loading from "./loading";
+import arrow from "../assets/arrow_back.svg";
 
 const Cards = ({ limit, categoryId, categoryName }) => {
   const [productos, setProductos] = useState([]);
@@ -22,33 +23,30 @@ const Cards = ({ limit, categoryId, categoryName }) => {
             console.error("Error al cargar los productos:", error);
             setIsLoading(false);
           });
-      }
-      catch (error) {
+      } catch (error) {
         console.error("Error al cargar los productos:", error);
         setIsLoading(false);
       }
-    }
+    };
     fetchProducts();
   }, [limit, categoryId]);
 
-
   useEffect(() => {
     function updateCardsToShow() {
-      if (window.innerWidth < 1024) {
+      desktopMediaQuery.addEventListener("change", updateCardsToShow);
 
+      if (window.innerWidth < 1024) {
         setCardsToShow([...productos].splice(0, productos.length - 2));
       } else {
-
         setCardsToShow([...productos]);
       }
     }
-
-    window.addEventListener("resize", updateCardsToShow);
+    const desktopMediaQuery = window.matchMedia("(min-width: 1024px)");
 
     updateCardsToShow();
 
     return () => {
-      window.removeEventListener("resize", updateCardsToShow);
+      window.removeEventListener("change", updateCardsToShow);
     };
   }, [productos]);
 
@@ -59,9 +57,15 @@ const Cards = ({ limit, categoryId, categoryName }) => {
   return (
     <section className="flex flex-col mt-4">
       <div className="">
-        <div className="flex justify-between">
+        <div className="flex justify-between my-2">
           <h2 className="text-2xl font-bold ">{categoryName}</h2>
-          <a href={`/categorias/${categoryId}`}>Ver mas --</a>
+          <a
+            className="flex items-center justify-center"
+            href={`/categorias/${categoryId}`}
+          >
+            Ver mas
+            <img src={`${arrow}`} alt="arrow" />
+          </a>
         </div>
         <article className="grid gap-2 grid-cols-2 md:grid-cols-4 lg:grid-cols-6 mx-auto">
           {cardsToShow.map((product) => (
